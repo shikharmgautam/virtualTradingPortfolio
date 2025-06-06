6a9545a5ea45f76cb972d10910240305a27cb07c
Zack-Kwalski <64278897+Zack-Kwalski@users.noreply.github.com>
2025-06-07 00:18:48 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/trade': 'http://localhost:3000',
@@ -8,5 +11,10 @@ export default defineConfig({
       '/positions': 'http://localhost:3000',
       '/stockdata': 'http://localhost:3000',
     }
+  },
+  preview: {
+    host: true,
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: 'all'
   }
 });
